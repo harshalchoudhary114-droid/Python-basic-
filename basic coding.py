db0b49c86b63446a671be0548e17e95122682450
Harshal <harshalchoudhary114@gmail.com>
2026-04-17 14:57:32 +0530

    Basic Coding
    #

---
+++ basic coding.py
@@ -0,0 +1,11 @@
+# Basic Algebraic equations using variables
+a=input("enter first number")
+b=input("enter second number")
+a=int(a)
+b=int(b)
+print(a+b)
+print(a-b)
+print(a%b)
+print(a**b)
+print(a/b)
+print(a*b)
